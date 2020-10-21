55b60fc58fef5408fd8276fd608d80d4d28ece97
Amy Gourlay <amy.gourlay@ntlworld.com>
2020-10-21 11:14:08 +0100

    Answer validation colouring & music fix
    Answers now turn green and red for right and wrong, wait 2 seconds then continues. Music also now plays continuously on all pages.

--- Website/components/GameMusic.vue
+++ Website/components/GameMusic.vue
@@ -1,7 +1,7 @@
 <template>
     <section class="sound">
         <audio id="audio" src="~/assets/Quizz.mp3" autoplay loop></audio>
-        <img class="img" src="~/assets/volume_on.png" @click="toggle" alt="Toggle Sound" height="120px" width="120px" align="right">
+        <img class="img" src="~/assets/volume_on.png" @click="toggle" alt="Toggle Sound" height="80px" width="80px" align="right">
     </section>
 </template>
 
--- Website/pages/index.vue
+++ Website/pages/index.vue
@@ -1,6 +1,6 @@
 <template>
 <div>
-  <GameMusic />
+  <!--<GameMusic />-->
   <section class="section">
     <div class="columns is-mobile">
       <!--<Header></Header>-->
