f359c0f0df734096eb03c1524ea43784a9984499
Sebastian <sebt32@rocketmail.com>
2020-10-21 13:29:44 +0100

    adjusted volume of music

--- Website/components/GameMusic.vue
+++ Website/components/GameMusic.vue
@@ -1,6 +1,6 @@
 <template>
     <section class="sound">
-        <audio id="audio" src="~/assets/Quizz.mp3" autoplay loop></audio>
+        <audio id="audio" src="~/assets/Quizz.mp3" v-on:canplay="start" loop></audio>
         <img class="img" src="~/assets/volume_on.png" @click="toggle" alt="Toggle Sound" height="80px" width="80px" align="right">
     </section>
 </template>
@@ -12,6 +12,13 @@ export default
     name: "GameMusic",
 
     methods: {
+        start()
+        {
+            var a = document.getElementById("audio")
+            a.volume = 0.2
+            a.play()
+        },
+
         toggle() 
         {
             if(!audio.paused)
