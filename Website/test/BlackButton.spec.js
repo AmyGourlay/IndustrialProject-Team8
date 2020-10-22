b93a95bf7ea98736e7b96d375d8aa7216ec9265c
Sebastian <sebt32@rocketmail.com>
2020-10-22 15:34:09 +0100

    Unit Tests for BlackButton component added

---
+++ Website/test/BlackButton.spec.js
@@ -0,0 +1,29 @@
+import { shallowMount } from '@vue/test-utils'
+import BlackButton from '@/components/BlackButton.vue'
+
+describe('Testing the BlackButton component', () => {
+
+  it('BlackButton component is mounted correctly', () => {
+    const wrapper = shallowMount(BlackButton,{
+      props: {
+        title: {
+          type: String,
+          required: true
+        }
+      }});
+    expect(wrapper.vm).toBeTruthy()
+  });
+
+  it("Test if title is added to button", () => {
+    const wrapper = shallowMount(BlackButton,{
+      props: {
+        title: {
+          type: String,
+          required: true
+        }
+      }});
+      wrapper.setData({ title: 'test_button'});
+      expect(wrapper.vm.title).toBe('test_button');
+    });
+
+});
